2cc31f3f022a27fa40bed4cb3e409b161ff34b9b
gurmaaan <dmitry@gurov.co>
2020-01-14 00:24:17 +0300

    Удален за ненадобностью проект C++ и оставлен только Qt  Designer файл

--- app/design_mainwindow.py
+++ app/design_mainwindow.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'C:\Users\Dima\PyFiles\MedNN\qt_cpp_project\mainwindow.ui'
+# Form implementation generated from reading ui file 'C:\Users\Dima\PyFiles\MedNN\app\mainwindow.ui'
 #
 # Created by: PyQt5 UI code generator 5.9.2
 #
--- ui_to_py_converter.bat
+++ ui_to_py_converter.bat
@@ -1,1 +1,1 @@
-pyuic5 C:\Users\Dima\PyFiles\MedNN\qt_cpp_project\mainwindow.ui -o C:\Users\Dima\PyFiles\MedNN\app\design_mainwindow.py
+pyuic5 C:\Users\Dima\PyFiles\MedNN\app\mainwindow.ui -o C:\Users\Dima\PyFiles\MedNN\app\design_mainwindow.py
